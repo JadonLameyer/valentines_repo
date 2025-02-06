3f9d34b932dc6c3f041a8a9784a6b5f24dc7e8b7
JadonLameyer <lmyjad001@myuct.ac.za>
2025-02-06 20:43:00 +0000

    chnaged a lot lol

--- script.js
+++ script.js
@@ -7,9 +7,11 @@ let clickCount = 0; // Counter to track how many times the button has been click
         document.getElementById("paragraph").innerHTML = "first just know that as your boyfriend, no matter how far away i am from you i will always do my best to put a smile on your face :)";
         
         var img =  document.getElementById("image");
+        
         img.src = "i_love_you_to_the_moon_and_back.jpg"; 
-        img.width = "350px"; 
-        img.height = "300px"; 
+        img.style.width = "350px"; 
+        img.style.height = "300px";
+        
 
         document.getElementById("body").style.background = "#ffdef1";
 
@@ -19,25 +21,28 @@ let clickCount = 0; // Counter to track how many times the button has been click
  
         var img =  document.getElementById("image");
         img.src = "man and hearts.jpg"; 
-        img.width= "417px"; 
-        img.height= "380px";  
-        document.getElementById("body").style.background = "#bba16e";
+        img.style.width = "350px"; 
+        img.style.height = "300px";
+        
+        // document.getElementById("body").style.background = "#bba16e";
     } 
       else if (clickCount === 3) {
         document.getElementById("paragraph").innerHTML =  "you are a shining light in my life and you deserve the world of happiness";
 
         var img =  document.getElementById("image");
         img.src = "dinosaur.jpg"; 
-        img.width= "417px"; 
-        img.height= "380px";  
+        img.style.width = "350px"; 
+        img.style.height = "300px";
+        
         document.getElementById("body").style.background = "#ffffff";
     } 
       else if (clickCount === 4) {
         document.getElementById("paragraph").innerHTML =  "you are really special to me nomthandazo, really special. i know that we haven't spent a lot of time with each other, but things will get better! i love you and i love us, and i will always choose you no matter the distance.";
         var img =  document.getElementById("image");
         img.src = "lipstick kisses.jpg"; 
-        img.width= "417px"; 
-        img.height= "380px";  
+        img.style.width = "350px"; 
+        img.style.height = "300px";
+         
         document.getElementById("body").style.background = "#white";
     } 
       else if (clickCount === 5) {
@@ -45,8 +50,9 @@ let clickCount = 0; // Counter to track how many times the button has been click
 
         var img =  document.getElementById("image");
         img.src = "for the best.jpg"; 
-        img.width= "417px"; 
-        img.height= "380px";  
+        img.style.width = "350px"; 
+        img.style.height = "300px";
+        
         document.getElementById("body").style.background = "white";
     } 
      else if (clickCount === 6) {
@@ -54,16 +60,18 @@ let clickCount = 0; // Counter to track how many times the button has been click
 
         var img =  document.getElementById("image");
         img.src = "rose and hearts.jpg";
-        img.width= "417px"; 
-        img.height= "380px";  
+        img.style.width = "350px"; 
+        img.style.height = "300px";
+         
         document.getElementById("body").style.background = "#96734d";
     } 
       else if (clickCount === 7) {
         document.getElementById("paragraph").innerHTML = "every single moment of every single day i think about you, you're always on my mind. you mean the absolute world to me and i can't wait to make so many more memories with you";
         var img =  document.getElementById("image");
         img.src = "youre_all_i_ever_wanted.jpg"; 
-        img.width= "417px"; 
-        img.height= "380px";  
+        img.style.width = "350px"; 
+        img.style.height = "300px";
+        
         document.getElementById("body").style.background = "#f7f1e3";
     } 
       else if (clickCount === 8) {
@@ -93,14 +101,14 @@ let clickCount = 0; // Counter to track how many times the button has been click
 
         // Add an event listener for click
         button.addEventListener("mouseenter", () => {
-            const maxX = window.innerWidth - 2500;
-            const maxY = window.innerHeight - 2500;
-
-            const randomX = Math.abs(Math.floor(Math.random() * maxX));
-            const randomY = Math.abs(Math.floor(Math.random() * maxY));
-
-            button.style.left = `${randomX}px`;
-            button.style.top = `${randomY}px`;
+          const maxX = window.innerWidth - button.clientWidth; // Prevent moving off-screen
+          const maxY = window.innerHeight - button.clientHeight;
+      
+          const randomX = Math.floor(Math.random() * maxX); 
+          const randomY = Math.floor(Math.random() * maxY);
+      
+          button.style.left = `${randomX}px`;
+          button.style.top = `${randomY}px`;
 
         });
 
@@ -108,8 +116,9 @@ let clickCount = 0; // Counter to track how many times the button has been click
         document.getElementById("paragraph").innerHTML = "so miss nomthandazo martha rametsi, will you be my valetine?";
         var img =  document.getElementById("image");
         img.src = "heart end.jpg"; 
-        img.width= "100%"; 
-        img.height= "100%"; 
+        img.style.width = "350px"; 
+        img.style.height = "300px";
+        
         img.style.objectFit = "contain"; 
         //2316 × 3088
         document.getElementById("body").style.background = "#F6F6F6";
